52b54a2c5eba8f57fb817fdc6cfd326b1a9567ad
samopro <samo.dn@gmail.com>
2020-08-05 16:59:13 +0200

    double jump added

--- Ninja.gd
+++ Ninja.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0, 0)
+var jumpCount = 0
+var onGround = true
 
 const GRAVITY = 35
 const SPEED = 300
@@ -23,8 +25,21 @@ func _physics_process(delta):
 		$NinSprite.play("idle")
 		velocity.x = 0
 
-	if not is_on_floor():
-		$NinSprite.play("jump")
+	#if not is_on_floor():
+		#$NinSprite.play("jump")
 		
 	if Input.is_action_just_pressed("ui_select"):
-		velocity.y = JUMPFORCE
+		if jumpCount < 1:
+			jumpCount += 1
+			velocity.y = JUMPFORCE
+			onGround = false
+			
+	if is_on_floor():
+		onGround = true
+		jumpCount = 0
+	else:
+		onGround = false
+		if velocity.y < 0:
+			$NinSprite.play("jump")
+		
+			
